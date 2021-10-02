ab3ff05289764c767fb723e0322c3a33760461f7
xorphitus <xorphitus@gmail.com>
2021-10-03 00:45:14 +0900

    add amchart

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,12 @@
 import React, {useEffect} from 'react';
+import * as am4charts from "@amcharts/amcharts4/charts";
+import * as am4core from "@amcharts/amcharts4/core";
+
 import './App.css';
 
 const canvasId = "cv";
 const data = (() => {
-    const cnt = 10000;
+    const cnt = 5000;
     const d = new Array(cnt);
     for (let i = 0; i < cnt; i++) {
         d[i] = Math.sin(i);
@@ -11,17 +14,47 @@ const data = (() => {
     return d;
 })();
 
+const amId = "am";
+const amData = data.map((value, i) => {
+    return {
+        i,
+        value
+    };
+});
+const getAmchart = (chartId) => {
+    const chart = am4core.create(chartId, am4charts.XYChart);
+    chart.zoomOutButton.disabled = true;
+    const xAxis = chart.xAxes.push(new am4charts.ValueAxis());
+    xAxis.renderer.minGridDistance = 60;
+    const yAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    const series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.valueX = "i";
+    series.dataFields.valueY = "value";
+    // disable initial effect for performance
+    series.showOnInit = false;
+    // set opacity to 1 for performance
+    series.fillOpacity = 1;
+    series.strokeOpacity = 1;
+    return {chart, xAxis, yAxis, series};
+};
+
 function App() {
     useEffect(() => {
         (async () => {
             const wasmDraw = (await import("wasm")).draw_wasm_line_chart;
             wasmDraw(canvasId, data);
         })();
+
+        const {chart} = getAmchart(amId);
+        chart.data = amData;
     });
 
     return (
         <div className="App">
             <canvas id={canvasId} width="600" height="400" />
+            <div id={amId} style={{"height": "100%"}}>
+                &nbsp;
+            </div>
         </div>
     );
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -35,7 +35,7 @@ fn draw(canvas_id: &str, data: &[f32]) -> DrawResult<impl Fn((i32, i32)) -> Opti
         .caption("this is a caption", font)
         .x_label_area_size(30)
         .y_label_area_size(30)
-        .build_cartesian_2d(0.2f32..10000.2f32, -1f32..1f32)?;
+        .build_cartesian_2d(0.2f32..5000.2f32, -1f32..1f32)?;
 
     chart.configure_mesh().x_labels(3).y_labels(3).draw()?;
     chart.draw_series(LineSeries::new(
