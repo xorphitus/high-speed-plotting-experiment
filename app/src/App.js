ff7e6e94ca3a1fd3b21d299877a656b5ae77fca8
xorphitus <xorphitus@gmail.com>
2021-10-02 23:44:23 +0900

    wasm integration
    see https://dev.to/lokesh007/webassembly-with-rust-and-react-using-create-react-app-67

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,14 @@
+import React, {  useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      import("wasm");
+    })();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
